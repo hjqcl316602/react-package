0d451d57e5d9394cf30622024087fb998d9e03c3
hjqcl316602 <415877608@qq.com>
2019-11-22 15:49:00 +0800

    update

--- src/index.js
+++ src/index.js
@@ -1,12 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-//import * as serviceWorker from './serviceWorker';
-
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-//serviceWorker.unregister();
+import Router from './router';
+import redux from './redux';
+import { Provider } from 'react-redux';
+import './style/index.less'
+ReactDOM.render(<Provider store={　redux }><Router /></Provider>, document.getElementById('root'));
